fba7f305452a543e85e3c9575a58c283af19d50b
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-10-02 01:19:01 +0530

    [Darshan]: add employee details in tables

--- ADOEmployeePayRoll/Program.cs
+++ ADOEmployeePayRoll/Program.cs
@@ -11,13 +11,17 @@
 
             void OptionsDisplay()
             {
-                Console.WriteLine("\nSelect option :\n1.Get Payroll Table Details\n6.Exit\nProvide option number : --> ");
+                Console.WriteLine("\nSelect option :\n1.Get Payroll Table Details\n2.Add data to tables\n6.Exit\nProvide option number : --> ");
                 int option = int.Parse(Console.ReadLine());
                 Console.WriteLine();
                 switch (option)
                 {
                     case 1:
                         emp.GetEmployeeDetails();
+                        OptionsDisplay();
+                        break;
+                    case 2:
+                        emp.AddEmpDetails();
                         OptionsDisplay();
                         break;
                     default:
