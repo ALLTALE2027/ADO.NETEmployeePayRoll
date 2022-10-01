1341426a1cc79faaacb1ddebd540503909969023
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-10-02 01:16:35 +0530

    [Darshan]: get employee all details from DB

---
+++ ADOEmployeePayRoll/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ADOEmployeePayRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Payroll Service program using ADO.NET \n");
+            EmployeeADO emp = new EmployeeADO();
+            OptionsDisplay();
+
+            void OptionsDisplay()
+            {
+                Console.WriteLine("\nSelect option :\n1.Get Payroll Table Details\n6.Exit\nProvide option number : --> ");
+                int option = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (option)
+                {
+                    case 1:
+                        emp.GetEmployeeDetails();
+                        OptionsDisplay();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option");
+                        break;
+                }
+            }
+
+        }
+    
+    }
+}
